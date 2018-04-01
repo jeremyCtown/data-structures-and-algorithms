1ffdcf78126ae41d4fa69919929bb1154332ff38
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-01 16:57:26 -0700

    this is a total cluster, stopping for real this time

--- challenges/largest-product/largest_product.py
+++ challenges/largest-product/largest_product.py
@@ -2,58 +2,35 @@
 # Inputs
 test_list = [[1, 2], [2, 3], [3, 4], [4, 5]]
 
-new_list = test_list
 
-
-def create_new_list(test_list):
+def multiply_inner_lists(test_list):
     """
-    creates a new list from 2D array
+    sums inner lists from existing list and returns a new list
     """
     counter = 0
-
-    for items in test_list:
-        print(test_list)
-        for i in items:
-            print(i)
-            new_list[counter] = i
-            print(new_list)
-            counter = counter + 1
-    # largest_product()
-    print(new_list)
-
-
-# a = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
-# for row in a:
-#     for elem in row:
-#         print(elem)
-#     print()
+    new_list = test_list
+    for i in test_list:
+        if len(i) > 1:
+            new_list[counter] = (i[0]*i[1])
+            counter += 1
+        else:
+            new_list[counter] = i[0]
+            counter += 1
+    largest_product(new_list)
+    return new_list
 
 
-# def largest_product(new_list):
-#     """
-#     loops through new_list and returns product of two numbers largest number
-#     """
-#     answer = 0
-#     for i in new_list:
-#         for item in i:
-#         product = new_list[i] * new_list[i+1]
-#         print(product)
-#         if answer < product:
-#             answer = product
-#     print(answer)
-#     return answer
+def largest_product(new_list):
+    """
+    loops through new_list and returns largest number
+    """
+    answer = 0
+    for i in new_list:
+        product = i * (i+1)
+        print(product)
+        if answer < product:
+            answer = product
+    print(answer)
+    return answer
 
-
-create_new_list(test_list)
-
-# n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
-# # Add your function here
-
-# def flatten(lists):
-#   results = []
-#   for numbers in lists:
-#     for number in numbers:
-#       results.append(number)
-#   return results
-
-# print flatten(n)
+multiply_inner_lists(test_list)
--- challenges/largest-product/test_largest_product.py
+++ challenges/largest-product/test_largest_product.py
@@ -1,22 +1,46 @@
 import pytest
-from largest_product import LargestProduct as LP
+import largest_product as LP
+
+test_list = [[1, 2], [2, 3], [3, 4], [4, 5]]
+new_list = [2, 6, 12, 20]
+answer = 420
+
+test_2 = [[1, 3], [5, 6], [1, 2], [0, 0]]
+new_2 = [3, 30, 2, 0]
+answer_2 = 90
+
+test_3 = [[5], [5]]
+new_3 = [5, 5]
+answer_3 = 25
 
 
-def test_create_new_list():
-    test_list = [[1, 2], [2, 3], [3, 4], [4, 5]]
-    assert LP.create_new_list(test_list) == [1, 2, 2, 3, 3, 4, 4, 5]
+def test_multiply_inner_lists_1():
+    assert LP.multiply_inner_lists(test_list) == new_list
 
 
-def test_multiply_inner_lists(test_lp):
-    test_lp.multiply_inner_lists()
-    assert test_lp.counter == 4
-    assert test_lp.new_list[0] is not str
-    assert test_lp.new_list == [2, 6, 12, 20]
+def test_multiply_inner_lists_2():
+    assert LP.multiply_inner_lists(test_2) == new_2
 
 
-def test_largest_product(test_lp):
-    test_lp.new_list = [2, 6, 12, 20]
-    test_lp.largest_product()
-    assert test_lp.new_list == [2, 6, 12, 20]
-    assert test_lp.answer is not str
-    assert test_lp.answer == 20
+def test_multiply_inner_lists_3():
+    assert LP.multiply_inner_lists(test_3) == new_3
+
+
+def test_largest_product_1():
+    assert LP.largest_product(new_list) == answer
+
+
+def test_largest_product_2():
+    assert LP.largest_product(new_2) == answer_2
+
+
+def test_largest_product_3():
+    assert LP.largest_product(new_3) == answer_3
+
+
+# def test_largest_product(test_lp):
+#     test_lp.new_list = [2, 6, 12, 20]
+#     test_lp.largest_product()
+#     assert test_lp.new_list == [2, 6, 12, 20]
+#     assert test_lp.answer is not str
+#     assert test_lp.answer == 20
