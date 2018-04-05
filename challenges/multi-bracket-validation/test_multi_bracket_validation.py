b7c873c53d906e2087441d833f003a0b642f8e28
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-04 21:12:11 -0700

    2 tests passing

--- challenges/multi-bracket-validation/test_multi_bracket_validation.py
+++ challenges/multi-bracket-validation/test_multi_bracket_validation.py
@@ -2,8 +2,12 @@ import pytest
 from multi_bracket_validation import multi_bracket_validation as mb
 
 stuff = '[]'
+broken_stuff = '{]'
 
 
 def test_for_stuff():
-
     assert mb(stuff) is True
+
+
+def test_for_broken_stuff():
+    assert mb(broken_stuff) is False
