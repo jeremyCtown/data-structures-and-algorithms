838a1e4b22fa1df44ff2e1b5669ef915ef8726f9
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-10 17:38:23 -0700

    code written, tests passing. updating readme

---
+++ challenges/fizzbuzztree/fizzbuzztree.py
@@ -0,0 +1,72 @@
+from node import Node
+
+def fizzbuzztree(val):
+    if (val % 15 == 0):
+        val = 'fizzbuzz'
+        return val
+    if (val % 5 == 0):
+        val = 'buzz'
+        return val
+    if (val % 3 == 0):
+        val = 'fizz'
+        return val
+    else:
+        return val
+
+
+class BST:
+    def __init__(self, iter=[]):
+        self.root = None
+        self.iter = iter
+
+        for item in self.iter:
+            self.insert(item)
+
+    def __repr__(self):
+        return '<BST Root {}>'.format(self.root.val)
+
+    def __str__(self):
+        return self.root.val
+
+    def insert(self, val):
+        node = Node(val)
+        current = self.root
+
+        if self.root is None:
+            self.root = node
+            return node
+
+        while current:
+            if val >= current.val:
+                if current.right is not None:
+                    current = current.right
+                else:
+                    current.right = node
+                    break
+
+            elif val < current.val:
+                if current.left is not None:
+                    current = current.left
+                else:
+                    current.left = node
+                    break
+
+        return node
+
+    def in_order(self, operation):
+
+        def _walk(node=None):
+            if node is None:
+                return None
+
+            if node is not None:
+                _walk(node.left)
+                # import pdb; pdb.set_trace()
+                node.val = operation(node.val)
+                _walk(node.right)
+
+        _walk(self.root)
+
+
+# BST.in_order(fizzbuzztree)
+
