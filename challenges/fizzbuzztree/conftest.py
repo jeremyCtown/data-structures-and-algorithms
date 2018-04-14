f3d5283eca636408fd7a452677d0a0cc89e30700
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-14 11:16:36 -0700

    remember to cd up to root before ACPing

--- challenges/fizzbuzztree/conftest.py
+++ challenges/fizzbuzztree/conftest.py
@@ -4,10 +4,16 @@ from bst import BST
 
 @pytest.fixture
 def test_bst():
+    """
+    Creates empty bst for testing
+    """
     return BST()
 
 
 @pytest.fixture
 def iterable():
+    """
+    creates branched BST for testing
+    """
     return BST([6, 3, 15, 1, 5, 8, 30])
 
