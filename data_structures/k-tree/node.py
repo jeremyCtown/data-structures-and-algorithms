dff5db030ce664e026a5fdb2d3540429488df9d7
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-23 20:50:12 -0700

    no idea if this is working, kind of over it

--- data_structures/k-tree/node.py
+++ data_structures/k-tree/node.py
@@ -1,12 +1,12 @@
 
 class Node(object):
     def __init__(self, val):
-        self._val = val
+        self.val = val
         self.children = []
 
     
     def __repr__(self):
-        return 'Val: {0} Children: {1}'.format(self.value, self.children)
+        return (self.val, self.children)
 
 
     def add_child(self, obj):
