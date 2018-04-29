d200774d379c47d1f16cf1e05d002ba4c47a1762
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-28 20:11:35 -0700

    ktree complete, need to add tests and queue

--- data_structures/k-tree/k_tree.py
+++ data_structures/k-tree/k_tree.py
@@ -3,106 +3,104 @@ from ../queue/queue import Queue
 from node import Node
 
 class KTree:
+    """
+    Create a new KTree.
+    """
 
     def __init__(self):
+        """Initialize KTree"""
         self.root = Node()
 
     def __repr__(self):
-        return self.root
+        """Magic repr."""
+        return 'Root Val: {}'.format(self.root.val)
 
     def __str__(self):
+        """Magic str."""
         return 'Root Val: {}'.format(self.root.val)
 
     def pre_order(self, operation):
         """
-        Operates on nodes starting at the root and 
-        moving left, then right
+        Operate on nodes starting at the root 
+        and moving left, then right.
         """
         def _walk(node=None):
+            if node is None:
+                return
 
-            if node is not None:
-                operation(node)
-                _walk(node.left)
-                _walk(node.right)
+            operation(node)
+            
+            for child in node.children:
+                _walk(child)
 
         _walk(self.root)
 
     def post_order(self, operation):
         """
-        Operates on nodes starting on left side and 
+        Operate on nodes starting on left side and 
         moving bottom to top, then switching to right side 
-        before moving to root
+        before moving to root.
         """
         def _walk(node=None):
+            if node is None:
+                return
 
-            if node is not None:
-                _walk(node.left)
-                _walk(node.right)
-                operation(node)
+            for child in node.children:
+                _walk(child)
+            
+            operation(node)
 
         _walk(self.root)
 
     def breadth_first_traversal(self, operation):
         """
-        Performs operation on ktree from top to bottom, left to right
+        Perform operation on ktree from top to bottom, left to right
         """
         q = Queue()
+        order = [] # purposed for testing
         q.enqueue(self.root)
-        test = [] # for testing purposes only
 
         while q:
             temp = q.front
             out = q.dequeue().val.val
             operation(out)
-            test.append(out) # for testing purposes only
+            order.append(out) # purposed for testing
 
             if temp.val.left:
                 q.enqueue(temp.val.left)
             if temp.val.right:
                 q.enqueue(temp.val.right)
-        return test
+        return order # purposed for testing
         
     
-    def insert(self, parent_val, val):
-        """
-        Inserts a new node into the tree
-        """
+    def insert(self, parent_val, val=None):
+        """Insert a new node into the KTree."""
         node = Node(val)
+        q = Queue()
+        touched = [] #  purposed for testing
 
         if self.root is None:
             self.root = node
+            self._size += 1
             return
 
         if parent_val is None:
             self.root.add_child(node)
+            return
+
+        q.enqueue(self.root)
+        while len(queue) > 0:
+            current = q.dequeue()
+            if current.val == parent_val:
+                current.add_child(node)
+                self._size += 1
+                return
+            touched.append(current.val) # purposed for testing
+            for child in current.children:
+                q.enqueue(child)
+        return touched # purposed for testing
         
         if current and not current.val:
             temp = None
-"""
-            for child in current.children:
-                if val[0] == child.val:
-                    temp = child
-                    break
 
-            if temp:
-                self.insert(val, temp)
-            else:
-                newNode = Node(val=val[0])
-                current.children.append(newNode)
-                self.insert(val, newNode)
-        else:
-            if len(val) > 1:
-                temp = None
-
-                for child in current.children:
-                    if val[1] == child.value:
-                        tmpChild = child
-                        break
-                
-                if tmpChild:
-                    self.insert(val[1:], tmpChild)
-                else:
-                    newNode = Node(value=val[1])
-                    current.children.append(newNode)
-                    self.insert(val[1:], newNode)
 
