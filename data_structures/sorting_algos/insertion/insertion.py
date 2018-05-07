a6c457f813d102de3386681d56b667dbda91753d
Jeremy Crawford <jfcraw08@gmail.com>
2018-05-07 14:11:49 -0700

    saving changes and switching branches

---
+++ data_structures/sorting_algos/insertion/insertion.py
@@ -0,0 +1,13 @@
+
+def insertion_sort(lst):
+    if len(lst) < 2:
+        return lst
+
+    for i in range(1, len(lst)):
+        while lst[i] < lst[i - 1]:
+            lst[i], lst[i - 1] = lst[i - 1], lst[i]
+            if (i - 1) == 0:
+                break
+            i -= 1
+
+    return lst
