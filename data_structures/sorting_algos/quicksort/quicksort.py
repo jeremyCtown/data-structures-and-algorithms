6a57b299f5144241b876be0f803d54ea9d5ca05c
Jeremy Crawford <jfcraw08@gmail.com>
2018-05-15 16:35:41 -0700

    quicksort is passing all tests, redme updated with image.. oops

--- data_structures/sorting_algos/quicksort/quicksort.py
+++ data_structures/sorting_algos/quicksort/quicksort.py
@@ -1,12 +1,19 @@
 
-
-
 def quicksort(lst):
     """Recursive function to order a list quickly."""
+    right = []
+    equal = []
+    left = []
+
     if len(lst) < 2:
         return lst
     else:
-        midpoint = len(lst)/2
-        L = quicksort(lst[midpoint::])
-        R = quicksort(lst[::midpoint])
-        return lst
+        pivot = lst[0]
+        for item in lst:
+            if item < pivot:
+                right.append(item)
+            if item == pivot:
+                equal.append(item)
+            if item > pivot:
+                left.append(item)
+        return quicksort(right)+equal+quicksort(left)
